59fa80712d5b525b095f3aaf0b396ea22ccaff95
Christopher N. Hesse <raymanfx@gmail.com>
2017-11-25 20:14:34 +0100

    wahoo: Build device tree overlay image
    Change-Id: Ia81db83b24286cbd2b4aaacb09a085d304f5c50a

--- BoardConfigAICP.mk
+++ BoardConfigAICP.mk
@@ -6,5 +6,6 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_CONFIG := flash_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/wahoo
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_NEEDS_DTBOIMAGE := true
 
 -include vendor/google/wahoo/BoardConfigVendor.mk
