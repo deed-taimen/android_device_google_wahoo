e8f12783deade58c9ecc6366666e77aec9323fd0
deed821 <deed821@gmail.com>
2018-05-13 13:00:42 +1000

    livedisplay: build services and update sepolicy
    Change-Id: Id5ee77fea5a64f45ca793831a8adc75ca3e3f8b4

--- device-lineage.mk
+++ device-lineage.mk
@@ -10,3 +10,11 @@ PRODUCT_PACKAGES += \
    com.android.ims.rcsmanager \
    RcsService \
    PresencePolling
+
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
