1c87e6d8561587e6e9b3b4cf2718e596c90d1fc0
deed821 <deed821@gmail.com>
2018-05-22 05:53:16 +1000

    remove dependencies and update clang version displayed
    Change-Id: I1d578c091cf2107246c70a1a2042277cec757c2f

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -13,7 +13,7 @@ TARGET_NEEDS_DTBOIMAGE := true
 TARGET_KERNEL_CLANG_TRIPLE := aarch64-linux-gnu-
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 7.0.2
-KBUILD_COMPILER_STRING :=
+KBUILD_COMPILER_STRING := Android (4751641 based on r328903) clang version 7.0.2 (based on LLVM 7.0.2svn)
 export KBUILD_COMPILER_STRING
 
 -include vendor/google/wahoo/BoardConfigVendor.mk
