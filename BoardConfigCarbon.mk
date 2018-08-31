deae07d23190b8d9920d554f1e746fe1f562fbce
deedwar <deed821@gmail.com>
2018-09-01 06:28:35 +1000

    wahoo: gzosp bringup

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -1,11 +1,12 @@
 # Custom Carbon BoardConfig for wahoo
 # Inline kernel building
 TARGET_KERNEL_CONFIG := carbon_wahoo_defconfig
-TARGET_KERNEL_SOURCE := kernel/google/wahoo
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_NEEDS_DTBOIMAGE := true
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_ARCH := arm64
 TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+TARGET_NEEDS_DTBOIMAGE := true
 
 # Audio
 BOARD_SUPPORTS_SOUND_TRIGGER := true
